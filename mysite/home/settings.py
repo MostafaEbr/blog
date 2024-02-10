b64de621b7786f0fcd9523b10faaea946f16dcb7
mostafa <mostafa@gmail.com>
2024-02-10 17:50:09 +0200

    update

--- mysite/home/settings.py
+++ mysite/home/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "posts",
+        'USER': "postgres",
+        'PASSWORD': "Mo12T2oF",
+        'HOST': "localhost",
+        'PORT': 5432
     }
 }
 # DATABASES = {
