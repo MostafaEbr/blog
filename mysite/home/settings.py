08d2e7d8c462ed87870ac972913358982e7de63b
mostafa <mostafa@gmail.com>
2024-02-10 18:06:26 +0200

    Revert "update"
    This reverts commit 2106b588a920360f05ffeddeb8f37bbcb0ca23c2.

--- mysite/home/settings.py
+++ mysite/home/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'home.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["docs"],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
